9bcd1cee0091527de4fae5210c4ede8354c80759
leo <facenord.sud@gmail.com>
2014-01-21 09:46:03 +0100

    First commit

---
+++ src/main/java/diuf/unifr/ch/xwot/arduino/components/door/ContiniousServo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package diuf.unifr.ch.xwot.arduino.components.door;
+
+/**
+ *
+ * @author leo
+ */
+public class ContiniousServo{
+    
+    private int speed;
+    public final static int NULL_SPEED = 93;
+    public final static int OPEN_MAX_SPEED = 105;
+    public final static int CLOSE_MAX_SPEED = 80;
+    
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
---
+++ src/main/java/diuf/unifr/ch/xwot/arduino/components/door/LinearPotentiometer.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diuf.unifr.ch.xwot.arduino.components.door;
+
+/**
+ * 0 mean fully open 1023 mean fully closed
+ *
+ * @author leo
+ */
+public class LinearPotentiometer {
+
+    private int oldPosition;
+    private int position;
+
+    public static final int OPEN_POSITION = 0;
+    public static final int CLOSED_POSITION = 1023;
+    public static final int MIDDLE_POSITION = 511;
+    public static final int ERROR = 15;
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public int getOldPosition() {
+        return oldPosition;
+    }
+
+    public void setOldPosition(int oldPosition) {
+        this.oldPosition = oldPosition;
+    }
+
+    /**
+     * The difference between the old and the new position of the door. the
+     * calcul is : oldPosition-newPosition
+     *
+     * @return smaller than 0 if door is closing
+     * <br/> 0 if door is not closing or not opening
+     * <br/> bigger than 0 if door is opening
+     */
+    public int getDifference() {
+        return oldPosition - position;
+    }
+
+    /**
+     * To know if the door is closing or not
+     *
+     * @return true if the door is closing. Else return false, as well when the
+     * door is not moving
+     */
+    public boolean isClosing() {
+        if (getDifference() < 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * The postition of the door in percent
+     *
+     * @return the position of the door in percent
+     */
+    public int getPercentPosition() {
+        return Math.round(((float)position/(float)CLOSED_POSITION)*100);
+    }
+
+    public void setFromPercentPosition(int percent) {
+        position = Math.round(((float)percent / (float)100) * 1023);
+    }
+
+    @Override
+    public String toString() {
+        return "LinearPotentiometer{" + "oldPosition=" + oldPosition + ", "
+                + "position=" + position + ", percentPosition=" + 
+                getPercentPosition() + ", isClosing=" + isClosing()+", difference="+getDifference();
+    }
+
+}
